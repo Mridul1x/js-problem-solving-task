d43619d5d6007e7639624453f77916b6788b6398
Mridul1x <mridulhasannn@gmail.com>
2023-07-27 14:23:50 +0600

    Second Problem Solved

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ const output = reverseString(input);
 console.log(output);
 
 
+
+
+
+// Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array.
+
+// Example Input: [2, -5, 10, -3, 7] Example Output: 19
+
+function sumOfPositiveNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+// Test the function
+const inputArray = [2, -5, 10, -3, 7];
+const output1 = sumOfPositiveNumbers(inputArray);
+console.log(output1); 
